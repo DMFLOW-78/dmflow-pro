8497506cd056a147026152d33461c4beaa428259
Marcelo <dmflow-pro@protonmail.com>
2026-04-08 10:17:30 -0300

    fix tailwind import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,18 @@ import React from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
-      <body>{children}</body>
-    </html>
+    <html lang="en">
+  <body className="flex flex-col min-h-screen">
+    
+    <main className="flex-1">
+      {children}
+    </main>
+
+    <footer className="text-center text-xs text-gray-400 py-3">
+  Criado por <span className="font-semibold">Marcelo Rebola</span>
+</footer>
+
+  </body>
+</html>
   );
 }
