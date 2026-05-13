29f978c2207582b613a1007725fcd3f45b3ac06b
Marcelo <dmflow-pro@protonmail.com>
2026-05-13 16:04:19 -0300

    redirect confirmed email to success page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -44,7 +44,7 @@ export default function RegisterPage() {
       email,
       password,
       options: {
-        emailRedirectTo: `${window.location.origin}/login`,
+        emailRedirectTo: `${window.location.origin}/email-confirmado`,
         data: {
           name,
           instagram,
