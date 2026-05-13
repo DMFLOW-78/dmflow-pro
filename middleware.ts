29b26a440d9459386423ce0071d49574ff032c04
Marcelo <dmflow-pro@protonmail.com>
2026-05-13 16:31:52 -0300

    Protecao dashboard middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get("sb-access-token");
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
   return NextResponse.next();
 }
 
